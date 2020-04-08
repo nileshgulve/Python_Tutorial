1c030a68c5dc8e09ab694f760aaab7cadd1883c8
Nilesh  Gulve <38161369+nileshgulve@users.noreply.github.com>
2020-04-08 10:28:48 -0700

    one line added

--- variable.py
+++ variable.py
@@ -1,2 +1,22 @@
 x = 10
 x
+print("hello python...")
+
+print(x)
+
+y = x + 5
+print(y)
+z  = x + y
+print(z)
+
+x = 9 #we can  value of defined   variable   ie. it is a mutable.
+
+#variable with Strings
+
+name = 'Nilesh'
+print(name)
+name1   =   name  + 'Gulve'
+print(name1)
+
+print(name[0])
+# String in python is imutable
